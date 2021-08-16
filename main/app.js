642918eb6ab3ff6e08f61697f08ee9a912bdb118
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2021-08-16 15:51:18 -0400

    [feat]: Download buttons

--- main/app.js
+++ main/app.js
@@ -1,1 +1,21 @@
+const downloadButtons = document.querySelectorAll(".download")
+
+// let btnDownload = document.getElementById('btn');
+// let img = document.getElementById('img1');
+downloadButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let imagePath = btn.getAttribute("download")
+        let fileName = getFileName(imagePath)
+        saveAs(imagePath, fileName);
+    })
+})
+btnDownload.addEventListener('click', () => {
+    let imagePath = img.getAttribute('src');
+    let fileName = getFileName(imagePath);
+    saveAs(imagePath, fileName);
+});
+
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/') + 1)
+}
 
